b55e96e9dac4a9f7e6f6bf42f33bc3588025297e
merisir573 <aikosstory@gmail.com>
2025-01-30 01:14:15 +0300

    Updated API routes

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.enableCors();
   await app.listen(3004);
 }
 bootstrap();
